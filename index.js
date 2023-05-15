d4efea5749aa8674a44c1fa6cf0f76a06705cbc0
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-05-14 22:39:33 -0400

    feat: add get method to start app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
-const PORT = process.env.PORT || 4200;
+const mongoose = require('mongoose');
 
 const app = express();
+app.get('/', (req, resp) => {
+  resp.send('app is working...');
+});
 
-app.listen(PORT, () => {
+app.listen(4200, () => {
   console.log('Server has been started on port 4200...');
 });
