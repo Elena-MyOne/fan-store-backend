890c922e5b5b6943aa02c5ad4702c5bc23451e5c
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-07-06 00:21:20 -0400

    fix: change confirm password GET request

--- index.js
+++ index.js
@@ -774,10 +774,9 @@ app.get('/users/:identifier/:password', (req, res) => {
   res.send(JSON.stringify(userWithoutPassword));
 });
 
-app.get('/users/:userId/:password', (req, res) => {
-  const id = Number(req.params.userId);
-  const providedPassword = req.params.password;
-  const user = users.find(user => user.id === id);
+app.get('/users/:email/:password', (req, res) => {
+   const { email: providedEmail, password: providedPassword } = req.params;
+  const user = users.find(user => user.email === providedEmail);
    if (!user) {
      res.status(404).send('User not found');
       return;
